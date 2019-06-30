e3411742015d1488f6a5864f8b5d424f1a8082e1
Nilli Martinez <52380654+nmsis@users.noreply.github.com>
2019-06-30 11:50:40 -0500

    Create Serial-Duplex.c
    Creates a serial duplex communication between the PIC and the computer, including the use of switches and LEDs

---
+++ Serial-Duplex.c
@@ -0,0 +1,19 @@
+char datoanalog;
+char datoserial;
+void main() {
+TRISB = 0; //Configuración PORTB como salida
+LATB = 0; //Limpiar PORT
+
+TRISD = 0xFF; //Configuración PORTD como entrada
+
+UART1_Init(9600);
+Delay_ms(100);
+while(1){
+
+if (UART1_Data_Ready()) {
+   datoserial = UART1_Read();      //Leo terminal
+   datoanalog = PORTD;             //Leo valor analógico
+   UART1_Write(datoanalog);        //Enviar a la terminal el valor analógico
+   LATB=datoserial;                //LED de terminal
+   Delay_ms(500);
+}}}
